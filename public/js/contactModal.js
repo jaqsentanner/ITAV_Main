2f4d88c9f6d30fe5fb8fd3796d7c659d41326ac2
Jaqsen <jtmeyer2014@gmail.com>
2023-04-03 15:35:30 -0400

    updated modal functionality, still in progress

---
+++ public/js/contactModal.js
@@ -0,0 +1,11 @@
+var contactModal = document.getElementById('contactModal');
+
+function modalShow() {
+    contactModal.setAttribute('style', 'display:block');
+    console.log('toast');
+};
+
+function closeModal() {
+    contactModal.setAttribute('style', 'display:none');
+};
+
